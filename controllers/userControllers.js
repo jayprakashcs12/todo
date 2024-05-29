973a1968b657a2f24d9f7b47b4c1de8f029966f7
Jay Prakash Singh <jayprakash199221@gmail.com>
2024-05-29 11:43:51 +0530

    latest code

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,33 +1,35 @@
-import User from "../models/User";
+import User from "../models/User.js";
+import { genToken } from "../utils/genToken.js";
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res, next) => {
-
     try {
-        
         const { name, email, password, confirmPassword } = req.body;
 
-        let existingUser = await User.findOne({email});
-
-        if(existingUser) {
-            return res.status(400).json({
-                message: "e-Mail exits already, try to login...!"
-            })
+        if (password !== confirmPassword) {
+            return res.status(400).json({ message: "Passwords do not match!" });
         }
 
+        let existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "e-Mail exists already, try to login...!" });
+        }
+
+        // Hash the password before saving it
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
         let newUser = await User.create({
-            name, email, password, confirmPassword
-        })
+            name, email, password: hashedPassword
+        });
 
-        let token = await jwt.sign({id:newUser._id}, "SecretKey", {
-            expiresIn: 24*60*60
-        })
+        let token = await genToken(newUser._id);
 
-        res.status(201).json(newUser, token)
-
+        res.status(201).json({ newUser, token });
     } catch (err) {
-
-        res.status(400).json({
-            message:err.message
-        })
+        res.status(400).json({ message: err.message });
     }
-}
+};
+
+// export const login = async 
--- models/User.js
+++ models/User.js
@@ -1,40 +1,46 @@
-import { Schema, model } from "mongoose";
+import { Schema,model } from "mongoose";
+import bcrypt from 'bcryptjs';
 
-let userSchema = new Schema(
-    {
-        name: {
-            type: String,
-            required: [true, "Name is required Field"],
-            minLength: [4, "Name should have at least 4 characters"],
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: [true, "Email is required Field"],
-            unique: true,
-            trim: true,
-        },
-        password: {
-            type: String,
-            required: [true, "Password is required Field"],
-            minLength: [8, "Password should have at least 8 characters"],
-            trim: true,
-        },
-        confirmPassword: {
-            type: String,
-            validate: {
-                validator: function (value) {
-                    return this.password === value
-                },
-                message: "Password and Confirm Password doesn't match",
-            },
-        },
+let userSchema=new Schema({
+    name:{
+        type:String,
+        required:[true,"Name is required Field"],
+        minLength:[4,"Name should have at least 4 characters"],
+        trim:true
     },
-    {
-        timestamps: true,
-    }
-);
+    email:{
+        type:String,
+        required:[true,"Email is required Field"],
+        unique:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required Field"],
+        minLength:[8,"Password should have at least 8 characters"],
+        trim:true
+    },
+    confirmPassword:{
+        type:String,
+        validate:{
+            validator:function(value) {
+                return this.password === value;
+            },
+            message:"Password and Confirm Password must be same...!"
+        },
+        select: false
+    },
+},{
+    timestamps:true
+})
+ 
+//pre hook --before saving the document
+userSchema.pre("save", async function(next){
+    let salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
 
-let User = model("User", userSchema);
+let User=model("User",userSchema)
 
 export default User;
