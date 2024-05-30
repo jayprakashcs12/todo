564154457391a215906c539cfdc40199a33327b6
Jay Prakash Singh <jayprakash199221@gmail.com>
2024-05-30 12:41:14 +0530

    updated code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { login, register } from '../controllers/userController.js';
+import { login, register } from '../controllers/userControllers.js';
 
 let userRouter=Router()
 
